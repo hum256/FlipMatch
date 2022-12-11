625b32f4addb4e0d1d2345bb54d6c6cc89bbfa70
hum256 <snth.hum@gmail.com>
2022-12-11 12:34:34 +0900

    [add]スタート画面，プレイ画面，クリア画面の追加

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,61 +1,276 @@
 # include <Siv3D.hpp> // OpenSiv3D v0.6.6
 
+const int SIZE = 5;
+const int WINDOW_WIDTH = ((SIZE+1)*2+1);
+const int WINDOW_HEIGHT = (SIZE+4);
+Grid<int> map(SIZE, SIZE);
+Grid<int> retry(SIZE, SIZE);
+Grid<int> start(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+
+
+class GameBoard
+{
+public:
+    int diffMapX = 32 * ((WINDOW_WIDTH - 1) / 2 + 1);
+    int diffMapY = 32 * (WINDOW_HEIGHT - SIZE) / 2;
+    int diffAnsX = 32 * ((WINDOW_WIDTH - 1) / 2 - SIZE);
+    int diffAnsY = 32 * (WINDOW_HEIGHT - SIZE) / 2;
+
+    int flip_num = 0;
+    int ans_flag = 0;
+
+    Array<Grid<int>> ans = {
+        {
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+        },
+        {
+            {0, 1, 0, 1, 0},
+            {1, 0, 1, 0, 1},
+            {0, 1, 0, 1, 0},
+            {1, 0, 1, 0, 1},
+            {0, 1, 0, 1, 0},
+        },
+        {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1},
+        }
+    };
+    
+    //
+    // 初期化
+    //
+    void Init()
+    {
+        flip_num = Random(3, 10);
+        ans_flag = Random(2);
+        CopyMatrix(ans[ans_flag], map);
+        for (int i=0; i<flip_num; i++) {
+            Flip(Random(SIZE-1), Random(SIZE-1));
+        }
+        CopyMatrix(map, retry);
+
+        for (int i=0; i<WINDOW_WIDTH; i++) {
+            for (int e=0; e<WINDOW_HEIGHT; e++) {
+                if (i>=diffAnsX/32 && i<=diffAnsX/32+SIZE*2 &&
+                    e>=diffAnsY/32 && e<=diffAnsY/32+SIZE-1) {
+                    start[i][e] = 2;
+                }
+                else {
+                    start[i][e] = Random(1);
+                }
+            }
+        }
+        for (int i=diffAnsX/32+2; i<=diffAnsX/32+SIZE*2-2; i++) {
+            start[i][diffAnsY/32+1] = 2;
+            start[i][diffAnsY/32+3] = 2;
+        }
+    }
+    
+    //
+    // スタート画面を表示
+    //
+    void DisplayStart()
+    {
+        ShowStart();
+        Font font{ 24 };
+        font(U"Mimic Lights out").drawAt(diffAnsX+3*32-8, diffAnsY+2*32-8, Color{234, 234, 234});
+        font(U"Click here to start").drawAt(diffAnsX+3*32-8, diffAnsY+4*32-8, Color{234, 234, 234});
+    }
+    
+    //
+    // プレイ中の画面を表示
+    //
+    void DisplayGame()
+    {
+        ShowMap();
+        ShowAns();
+        CheckClear();
+        
+        Rect{diffAnsX, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+        Rect{diffAnsX+32, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+        Rect{diffAnsX+(SIZE-1)*32, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+        Rect{diffAnsX+(SIZE-0)*32, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+        Rect{diffAnsX+(SIZE+1)*32, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+        Rect{diffMapX+3*32, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+        Rect{diffMapX+4*32, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+
+        Font font24{ 24 };
+        font24(U"Target").drawAt(diffAnsX+2*32-8, diffAnsY-8, Color{234, 234, 234});
+        font24(U"Player").drawAt(diffMapX+1*32-2, diffAnsY-8, Color{234, 234, 234});
+        Font font18{ 18 };
+        font18(U"Return").drawAt(diffAnsX+4, diffAnsY+(SIZE+1)*32-8, Color{37, 42, 52});
+        font18(U"New game").drawAt(diffAnsX+4*32+4, diffAnsY+(SIZE+1)*32-8, Color{37, 42, 52});
+        font18(U"Retry").drawAt(diffAnsX+9*32+10, diffAnsY+(SIZE+1)*32-8, Color{37, 42, 52});
+    }
+    
+    //
+    // クリア画面を表示
+    //
+    void DisplayClear()
+    {
+        ShowMap();
+        ShowAns();
+        CheckClear();
+
+        for (int i=diffAnsX/32; i<=diffAnsX/32+SIZE*2; i++) {
+            Rect{i*32, diffAnsY+SIZE*32, 32}.draw(Color{99, 214, 213});
+        }
+
+        Font font24{ 24 };
+        font24(U"Target").drawAt(diffAnsX+2*32-8, diffAnsY-8, Color{37, 42, 52});
+        font24(U"Player").drawAt(diffMapX+1*32-2, diffAnsY-8, Color{37, 42, 52});
+        Font font18{ 18 };
+        font18(U"Game clear!!   :   Click here to Start menu").drawAt(diffAnsX+8, diffAnsY+(SIZE+1)*32-8, Color{234, 234, 234});
+    }
+    
+private:
+    //
+    // マップをコピーする
+    //
+    void CopyMatrix(Grid<int> from, Grid<int> to)
+    {
+        int i, e;
+        to = from;
+
+        for (i=0; i<SIZE; i++) {
+            for (e=0; e<SIZE; e++) {
+                to[i][e] = from[i][e];
+            }
+        }
+    }
+    
+    //
+    // 反転処理
+    //
+    void Flip(int x, int y)
+    {
+        Console << U"flip[" << x << U"][" << y << U"]";
+        map[x][y] = not map[x][y];
+        for (int i=x; i<SIZE; i++)    map[i][y] = not map[i][y];
+        for (int i=x; i >= 0; i--)    map[i][y] = not map[i][y];
+        for (int i=y; i<SIZE; i++)    map[x][i] = not map[x][i];
+        for (int i=y; i >= 0; i--)    map[x][i] = not map[x][i];
+    }
+    
+    //
+    // スタート画面の背景を表示
+    //
+    void ShowStart()
+    {
+        for (int i=0; i<WINDOW_WIDTH; i++) {
+            for (int e=0; e<WINDOW_HEIGHT; e++) {
+                switch (start[i][e]) {
+                    case 0:
+                        Rect{i*32, e*32, 32}.draw(Color{234, 234, 234});
+                        break;
+                    case 1:
+                        Rect{i*32, e*32, 32}.draw(Color{234, 70, 102});
+                        break;
+                    case 2:
+                        Rect{i*32, e*32, 32}.draw(Color{37, 42, 52});
+                        break;
+                    case 3:
+                        Rect{i*32, e*32, 32}.draw(Color{99, 214, 213});
+                        break;
+                }
+            }
+        }
+    }
+    
+    //
+    // マップを表示
+    //
+    void ShowMap()
+    {
+        for (int i=0; i<SIZE; i++) {
+            for (int e=0; e<SIZE; e++) {
+                switch (map[i][e]) {
+                    case 0:
+                        Rect{i*32+diffMapX, e*32+diffMapY, 32}.draw(Color{234, 234, 234});
+                        break;
+                    case 1:
+                        Rect{i*32+diffMapX, e*32+diffMapY, 32}.draw(Color{234, 70, 102});
+                        break;
+                }
+            }
+        }
+    }
+
+    //
+    // 答えを表示
+    //
+    void ShowAns()
+    {
+        for (int i=0; i<SIZE; i++) {
+            for (int e=0; e<SIZE; e++) {
+                switch (ans[ans_flag][i][e]) {
+                    case 0:
+                        Rect{i*32+diffAnsX, e*32+diffAnsY, 32}.draw(Color{234, 234, 234});
+                        break;
+                    case 1:
+                        Rect{i*32+diffAnsX, e*32+diffAnsY, 32}.draw(Color{234, 70, 102});
+                        break;
+                }
+            }
+        }
+    }
+    
+    //
+    // ゲーム終了を判定
+    //
+    bool CheckClear()
+    {
+        bool hasClear = true;
+        for (int i=0; i<SIZE&&hasClear; i++) {
+            for (int e=0; e<SIZE; e++) {
+                if (ans[ans_flag][i][e] != map[i][e]) {
+                    hasClear = false;
+                    break;
+                }
+            }
+        }
+        Scene::SetBackground(ColorF{ 0.031, 0.851, 0.839, 1.0 });
+        return hasClear;
+    }
+};
+
+
+
+
+
 void Main()
 {
-	// 背景の色を設定 | Set background color
-	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
-
-	// 通常のフォントを作成 | Create a new font
-	const Font font{ 60 };
-
-	// 絵文字用フォントを作成 | Create a new emoji font
-	const Font emojiFont{ 60, Typeface::ColorEmoji };
-
-	// `font` が絵文字用フォントも使えるようにする | Set emojiFont as a fallback
-	font.addFallback(emojiFont);
-
-	// 画像ファイルからテクスチャを作成 | Create a texture from an image file
-	const Texture texture{ U"example/windmill.png" };
-
-	// 絵文字からテクスチャを作成 | Create a texture from an emoji
-	const Texture emoji{ U"🐈"_emoji };
-
-	// 絵文字を描画する座標 | Coordinates of the emoji
-	Vec2 emojiPos{ 300, 150 };
-
-	// テキストを画面にデバッグ出力 | Print a text
-	Print << U"Push [A] key";
-
+    Window::Resize(WINDOW_WIDTH*32, WINDOW_HEIGHT*32);
+    Window::SetTitle(U"Mimic Lights Out");
+    Scene::SetBackground(ColorF{ 0.145, 0.165, 0.204, 1.0 });
+    
+    GameBoard gameBoard;
+    gameBoard.Init();
+    
+    int game_state = 2;
 	while (System::Update())
 	{
-		// テクスチャを描く | Draw a texture
-		texture.draw(200, 200);
-
-		// テキストを画面の中心に描く | Put a text in the middle of the screen
-		font(U"Hello, Siv3D!🚀").drawAt(Scene::Center(), Palette::Black);
-
-		// サイズをアニメーションさせて絵文字を描く | Draw a texture with animated size
-		emoji.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(emojiPos);
-
-		// マウスカーソルに追随する半透明な円を描く | Draw a red transparent circle that follows the mouse cursor
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1, 0, 0, 0.5 });
-
-		// もし [A] キーが押されたら | When [A] key is down
-		if (KeyA.down())
-		{
-			// 選択肢からランダムに選ばれたメッセージをデバッグ表示 | Print a randomly selected text
-			Print << Sample({ U"Hello!", U"こんにちは", U"你好", U"안녕하세요?" });
-		}
-
-		// もし [Button] が押されたら | When [Button] is pushed
-		if (SimpleGUI::Button(U"Button", Vec2{ 640, 40 }))
-		{
-			// 画面内のランダムな場所に座標を移動
-			// Move the coordinates to a random position in the screen
-			emojiPos = RandomVec2(Scene::Rect());
-		}
+        if (game_state == 0) {
+            gameBoard.DisplayStart();
+        }
+        else if (game_state == 1) {
+            gameBoard.DisplayGame();
+        }
+        else if (game_state == 2) {
+            gameBoard.DisplayClear();
+        }
 	}
 }
+
+
 
 //
 // = アドバイス =
